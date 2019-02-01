9b109a7772ee6f32b87f38c6fc05638b4a393ab4
qiu997018209 <997018209@qq.com>
2019-02-01 20:34:21 +0800

    修改

--- lib/my/questionPage.dart
+++ lib/my/questionPage.dart
@@ -10,7 +10,7 @@ class questionPage extends StatelessWidget {
       decoration: new BoxDecoration(
           borderRadius: BorderRadius.circular(8.0),
           border: Border.all(
-            color: Colors.grey,
+            color: color,
             width: 0.5,
           )
       ),
@@ -32,7 +32,6 @@ class questionPage extends StatelessWidget {
               style: TextStyle(
                   color: Colors.black,
                   fontSize: 14.0,
-                  fontWeight: FontWeight.bold
               ),
             ),
           ),
@@ -57,6 +56,7 @@ class questionPage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _buildTaskItem(Colors.green,"问:如何改头像","答:点击发布->修改头像"),
+                _buildTaskItem(Colors.blue,"问:91视频在哪里看","答:点击发现->91视频"),
                 _buildTaskItem(Colors.blue,"问:夫妻币有什么用?,如何获得?","答:查看一个联系方式或者聊天需要消耗一枚夫妻币,方式一:邀请夫妻加入,填写你的邀请码,即可获赠10枚夫妻币,方式二:联系客服充值,方式三:认证夫妻,前三个月每月赠送100枚"),
                 _buildTaskItem(Colors.yellow,"问:在哪里获得我的邀请码","答:点击我->推广赚取夫妻币"),
                 _buildTaskItem(Colors.pinkAccent,"问:夫妻如何认证","答:点击”我“,在头像下方查看ID,女方手持一张纸,纸上写你的ID,拍照给客服,即可开通,单男不需要认证"),
